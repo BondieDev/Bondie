058cf3fd3ce1f1e8c44c4f8067ada5d6fb5ade56
BondieDev <djdaankoning@gmail.com>
2021-12-03 13:48:45 +0100

    removed all the stinking text

--- bot.js
+++ bot.js
@@ -1,16 +1,11 @@
-// Load up the discord.js library
+
 const Discord = require("discord.js");
 
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
 const client = new Discord.Client();
-// test
-// Here we load the config.json file that contains our token and our prefix values. 
+
 const config = require("./config.json");
 const fs = require("fs");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+
 client.on("ready", () => {
 
 	let serverlist = ''
@@ -19,10 +14,9 @@ client.on("ready", () => {
     count += guild.memberCount // <---
 
 })
-  // This event will run if the bot starts, and logs in, successfully.
+  
   console.log(`Bot has started, with ${client.guilds.cache.size} servers, ${client.channels.cache.size} channels and ${count} users.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
+  
   let statuses = [ `${count} users!`, `Prefix : -`, `ItsBondie`];
 
   setInterval(function(){
@@ -40,21 +34,19 @@ client.on("ready", () => {
 });
 
 client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
+  
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 });
 
 client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
+
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
 
 client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
+
   console.log("[" + message.channel.name + "] " + message.member.user.tag + " > " + message.content);
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;  
   if(message.content.indexOf(config.prefix) !== 0) return;
   
@@ -63,8 +55,6 @@ client.on("message", async message => {
 
   // Ping command
   if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     let ping = (Math.round(Math.random() * 17) + 2)
     let ping2 = (Math.round(Math.random() * 12) + 1)
     message.channel.send("Pong?").then((message)=>{
