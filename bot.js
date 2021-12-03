9edd43549f2b213ae2d26fc146ba22f157d366be
BondieDev <djdaankoning@gmail.com>
2021-12-03 09:52:39 +0100

    Removed shitty text

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./config.json');
-// Configure logger settings
+
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
@@ -18,8 +17,6 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `-`
     if (message.substring(0, 1) == '-') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -31,7 +28,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
-                                    });
+                                    
+                });
             break;
             // Just add any case commands if you want to..
          }
