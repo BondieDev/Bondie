33d81a7a93ac3f83035e9f0df61678dfa3584665
BondieDev <djdaankoning@gmail.com>
2021-12-07 10:35:27 +0100

    Added -say command
    Made some changes, usually fixes, unless stated in Summary

--- bot.js
+++ bot.js
@@ -71,13 +71,14 @@ client.on("message", async message => {
 
 
   if (command === "say"){
+  	message.delete();
   	let MSG = message.content.split(" ");
   	let Query = MSG.slice(1).join("+");
   	let QueryD = MSG.slice(1).join(" ");
   	if (!Query) message.reply("Please specify something for me to say!")
   	else
   {
-    message.channel.send(QueryD + " -" + message.author.tag)
+    message.channel.send(QueryD)
 }
 }
 
